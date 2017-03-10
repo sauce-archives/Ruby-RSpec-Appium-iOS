bd94d89121b507ff34e19f4adf8758e7ad83621f
ndmanvar <ndmanvar@yahoo.com>
2017-03-09 16:07:36 -0800

    showing how to repro

--- spec/test1_spec.rb
+++ spec/test1_spec.rb
@@ -4,17 +4,11 @@ describe "Appium Test" do
   it "should test the app" do
   	# populate text fields with values
   	field_one = @driver.find_element(:accessibility_id, "TextField1")
-  	field_one.send_keys("12")
 
-  	field_two = @driver.find_elements(:class_name, "UIATextField")[1]
-  	field_two.send_keys("8")
-
-  	# trigger computation by using the button
-    @driver.find_element(:accessibility_id, "ComputeSumButton").click();
-
-    # is sum equal?
-    sum = @driver.find_element(:class_name, "UIAStaticText").text();
-    expect(Integer(sum)).to eq(20)
+    for i in 1..100
+      field_one.send_keys("12345567890")
+      field_one.clear()
+    end
 
   end
 end
